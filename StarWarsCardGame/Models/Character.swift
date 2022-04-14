ecc559d87787833bfe522655fe2d0b62c609504f
TasukuY <alohaty81@gmail.com>
2022-04-14 12:15:31 -0600

    initial commit

---
+++ StarWarsCardGame/Models/Character.swift
@@ -0,0 +1,29 @@
+//
+//  Character.swift
+//  StarWarsCardGame
+//
+//  Created by Tasuku Yamamoto on 4/14/22.
+//
+
+import UIKit
+
+class Character {
+    //MARK: - Properties
+    let name: String
+    let photo: UIImage
+    let faction: String
+    
+    //MARK: - Initializer
+    init(name: String, photo: UIImage, faction: String){
+        self.name = name
+        self.photo = photo
+        self.faction = faction
+    }
+    
+}//End of class
+
+extension Character: Equatable{
+    static func == (lhs: Character, rhs: Character) -> Bool {
+        return lhs.name == rhs.name && lhs.photo == rhs.photo && lhs.faction == rhs.faction
+    }
+}
